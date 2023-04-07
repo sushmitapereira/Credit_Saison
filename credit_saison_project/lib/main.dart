5c52e690c41b8b32b0d39fc598a24f8cbf101318
sushmitapereira <sushmitapereira@gmail.com>
2023-04-07 12:28:53 +0530

    hi

--- credit_saison_project/lib/main.dart
+++ credit_saison_project/lib/main.dart
@@ -1,99 +1,116 @@
-// Importing important packages require to connect
-// Flutter and Dart
+// main.dart
 import 'package:flutter/material.dart';
 import 'package:credit_saison_project/constants.dart';
+import 'package:credit_saison_project/my_flutter_app_icons.dart';
+import 'package:pie_chart/pie_chart.dart';
 
-// Main Function
 void main() {
-// Giving command to runApp() to run the app.
-
-/* The purpose of the runApp() function is to attach
-the given widget to the screen. */
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
-// Widget is used to create UI in flutter framework.
-
-/* StatelessWidget is a widget, which does not maintain
-any state of the widget. */
-
-/* MyApp extends StatelessWidget and overrides its
-build method. */
 class MyApp extends StatelessWidget {
-const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-// This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-  
-	return MaterialApp(
-    
-	// title of the application
-   debugShowCheckedModeBanner: false,
-	title: 'Hello World Demo Application',
-	// theme of the widget
-	theme: ThemeData(
-    
-	//	primarySwatch: Colors.lightGreen,
-	),
-	// Inner UI of the application
-	home: const MyHomePage(title: ''),
-	);
-}
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      // Remove the debug banner
+      debugShowCheckedModeBanner: false,
+      title: 'Kindacode.com',
+      home: HomePage(),
+    );
+  }
 }
 
-/* This class is similar to MyApp instead it
-returns Scaffold Widget */
-class MyHomePage extends StatelessWidget {
-const MyHomePage({Key? key, required this.title}) : super(key: key);
-final String title;
-
-@override
-Widget build(BuildContext context) {
+class HomePage extends StatelessWidget {
   
-	return Scaffold(
-    backgroundColor: kBackgroundDarkColor,
-	appBar: AppBar(
-  leading: Icon(Icons.menu,
-          size: 20,
-          color: kIconColor,),
-          actions: [
-           // Icon(Icon.)
-          ],
-  backgroundColor: Colors.transparent,
-   elevation: 0,
-		title: Text(title),
-	),
-	// Sets the content to the
-	// center of the application page
-	body:Container(
-    
-    padding: const EdgeInsets.only( top: 0, left: 10, right: 10),
-    decoration: BoxDecoration(
-      gradient: LinearGradient(colors: 
-      [
-        kBackgroundDarkColor,
-        kBackgroundMidColor,
-        kBackgroundLightColor,
-      ],
-      stops: [0.01,0.08,1,],
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+     Map<String, double> dataMap = {
+   "Utilised_Limit":25,
+   "Available_Limit":75,
+  };
+    List<Color> colorList = [
+     const Color(0xff3EE094),
+    Color.fromARGB(255, 255, 255, 255),
+  
+   
+  ];
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+             
+              colors: [kBackgroundDarkColor,kBackgroundMidColor, kBackgroundLightColor],
+                stops: [0.01,0.08,1,],
       begin: Alignment.topCenter,
       end: Alignment.bottomCenter,
-      )
-    ),
-  child: Column(
-    children: [
-      Row(
-        children: [
-          //Icon(Icons.menu,
-          //size: 20,
-         // color: kIconColor,)
+      )),
+      child: Scaffold(
+          // By defaut, Scaffold background is white
+          // Set its value to transparent
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+           leading: IconButton(onPressed: (){
 
-        ],
-      )
-    ],
-  ),
-  )
-	);
-}
+           }, icon: Icon(MyFlutterApp.menu)),
+           actions: [IconButton(onPressed: (){
+
+           }, icon:Icon(MyFlutterApp.notify))],
+          ),
+          body:SingleChildScrollView(
+              
+              child: Column(
+               
+                children: <Widget>[
+                 
+                  Align(
+                    alignment: AlignmentDirectional.centerStart,
+                    child: Container(
+                    
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.horizontal(
+                            right: Radius.circular(60),
+                          
+                          ),
+                          color: kTopProcessFeeColor,
+                          
+                          ),
+                  child: Center(
+                     
+                        child:Row(children: <Widget>[
+                          
+                          Icon(MyFlutterApp.offer,color: Colors.white,),
+                          Text('  0% Processing Fee',style: TextStyle(fontSize: 15,color: Colors.white),)
+                        ],),
+                        
+                      ),
+                      height: 80,
+                      width: 300,
+                    
+                    ),
+                    
+                  ),
+                 Center(
+                    
+                    child: PieChart(dataMap: dataMap,
+                    colorList:colorList,
+                    chartRadius: MediaQuery.of(context).size.width/2,
+                    ringStrokeWidth: 24,
+                    animationDuration: const Duration(seconds: 3),
+                    chartType: ChartType.ring,
+                    
+                    ),
+                        
+                  ),
+              ],
+              ),
+            ),
+             
+          ),
+    );
+  }
 }
